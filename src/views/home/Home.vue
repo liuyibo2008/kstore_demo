0811bab88f1933388d7c023883510d2622eb86d0
liuyibo2008 <liuyibo2008@126.com>
2020-05-27 14:06:58 +0800

    提交顶部导航

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,51 @@
 <template>
-    <h2>首页</h2>
+    <div id="home">
+        <nav-bar class="nav-bar">
+            <div slot="center">购物街</div>
+        </nav-bar>
+    </div>
+    
 </template>
 
 <script>
+    import NavBar from 'components/common/navbar/NavBar'
     export default {
-        name: "Home"
+        name: "Home",
+        components:{
+            NavBar
+        }
     }
 </script>
 
 <style scoped>
+  #home {
+    /*position: relative;*/
+    height: 100vh;
+  }
+  .nav-bar {
+    background-color: var(--color-tint);
+    font-weight: 700;
+    color: #fff;
+  }
 
+  .content {
+    position: absolute;
+    top: 44px;
+    bottom: 49px;
+    left: 0;
+    right: 0;
+  }
+
+  .fixed {
+    position: fixed;
+    top: 44px;
+    left: 0;
+    right: 0;
+  }
+
+  .back-top {
+    position: fixed;
+    right: 10px;
+    bottom: 60px;
+  }
 </style>
